805111e8f037bb5896362fcec7fc8d1dcb1e7527
jackwhiters <jackwhiters55@gmail.com>
2021-12-28 20:08:51 +0700

    fix error sendcode

--- assets/php/actions.php
+++ assets/php/actions.php
@@ -1,219 +1,210 @@
 <?php
 require_once 'functions.php';
+//dibuat setelah menggunakan PHPMailer
 require_once 'send_code.php';
+// print_r($_POST);
 
-
-if(isset($_GET['test'])){
- 
+//untuk mengunset lupa password
+if(isset($_GET['newfp']))
+{
+    unset($_SESSION['auth_temp']);
+    unset($_SESSION['lupa_email']);
+    unset($_SESSION['lupa_code']);
 }
-
-if(isset($_GET['block'])){
-    $user_id = $_GET['block'];
-    $user = $_GET['username']; 
-      if(blockUser($user_id)){
-          header("location:../../?u=$user");
-      }else{
-          echo "something went wrong";
-      }
-  
-    
-  }
-
-  if(isset($_GET['deletepost'])){
-    $post_id = $_GET['deletepost'];
-      if(deletePost($post_id)){
-          header("location:{$_SERVER['HTTP_REFERER']}");
-      }else{
-          echo "something went wrong";
-      }
-  
-    
-  }
-
-
-
-//for managaing signup
-if(isset($_GET['signup'])){
-$response=validateSignupForm($_POST);
-if($response['status']){
-    if(createUser($_POST)){
-    header('location:../../?login&newuser');
-    }else{
-        echo "<script>alert('somethihng is wrong')</script>";
-    }
-   
-
-}else{
-    $_SESSION['error']=$response;
-    $_SESSION['formdata']=$_POST;
-    header("location:../../?signup");
-}
-    
-}
-
-
-
-//for managing login
-if(isset($_GET['login'])){
-
-  
-    $response=validateLoginForm($_POST);
-  
-    if($response['status']){
-     $_SESSION['Auth'] = true;
-     $_SESSION['userdata'] = $response['user'];
-
-     if($response['user']['ac_status']==0){
-     $_SESSION['code']=$code = rand(111111,999999);
-     sendCode($response['user']['email'],'Verify Your Email',$code);
-     }
-
-     header("location:../../");
-
-    }else{
-        $_SESSION['error']=$response;
-        $_SESSION['formdata']=$_POST;
-        header("location:../../?login");
-    }
-        
-    }
-
-
-    if(isset($_GET['resend_code'])){
-       
-            $_SESSION['code']=$code = rand(111111,999999);
-            sendCode($_SESSION['userdata']['email'],'Verify Your Email',$code);
-            header('location:../../?resended');
-    }
-
-    if(isset($_GET['verify_email'])){
-       $user_code = $_POST['code'];
-       $code = $_SESSION['code'];
-       if($code==$user_code){
-       if(verifyEmail($_SESSION['userdata']['email'])){
-        header('location:../../');
-       }else{
-           echo "something is wrong";
-       }
-
-       }else{
-           $response['msg']='incorrect verifictaion code !';
-           if(!$_POST['code']){
-            $response['msg']='enter 6 digit code !';
-
-           }
-           $response['field']='email_verify';
-        $_SESSION['error']=$response;
-        header('location:../../');
-
-       }
-       
-}
-
-
-if(isset($_GET['forgotpassword'])){
-    if(!$_POST['email']){
-        $response['msg']="enter your email id !";
-        $response['field']='email';
-        $_SESSION['error']=$response;
-        header('location:../../?forgotpassword');
-
-    }elseif(!isEmailRegistered($_POST['email'])){
-        $response['msg']="email id is not registered";
-        $response['field']='email';
-        $_SESSION['error']=$response;
-        header('location:../../?forgotpassword');
-
-    }else{
-          $_SESSION['forgot_email']=$_POST['email'];
-           $_SESSION['forgot_code']=$code = rand(111111,999999);
-            sendCode($_POST['email'],'Forgot Your Password ?',$code);
-            header('location:../../?forgotpassword&resended');
-    }
-
-
-}
-
-
-
-//for logout the user
-if(isset($_GET['logout'])){
-    session_destroy();
-    header('location:../../');
-
-}
-
-
-// for verify forgot code
-if(isset($_GET['verifycode'])){
-    $user_code = $_POST['code'];
-    $code = $_SESSION['forgot_code'];
-    if($code==$user_code){
-    $_SESSION['auth_temp']=true;
-     header('location:../../?forgotpassword');
-    }else{
-        $response['msg']='incorrect verifictaion code !';
-        if(!$_POST['code']){
-         $response['msg']='enter 6 digit code !';
-
-        }
-        $response['field']='email_verify';
-     $_SESSION['error']=$response;
-     header('location:../../?forgotpassword');
+//fungsi manajement daftar
+if(isset($_GET['daftar']))
+{
+   $response=validateDaftarForm($_POST);
+   print_r($response);
+   if($response['status'])
+   {
+    if(createUser($_POST))
+    {
+        header('location:../../?login');
 
     }
-    
-}
-
-if(isset($_GET['changepassword'])){
-    if(!$_POST['password']){
-        $response['msg']="enter your new password";
-        $response['field']='password';
-        $_SESSION['error']=$response;
-        header('location:../../?forgotpassword');
-    }else{
-        resetPassword($_SESSION['forgot_email'],$_POST['password']);
-        session_destroy();
-        header('location:../../?reseted');
+    else
+    {
+        echo "<script>alert ('ada yang salah')</script>";
     }
-
-
-}
-
-
-if(isset($_GET['updateprofile'])){
-
-    $response=validateUpdateForm($_POST,$_FILES['profile_pic']);
-
-    if($response['status']){
-       
-        if(updateProfile($_POST,$_FILES['profile_pic'])){
-            header("location:../../?editprofile&success");
-
-        }else{
-            echo "something is wrong";
-        }
-       
-    
-    }else{
-        $_SESSION['error']=$response;
-        header("location:../../?editprofile");
-    }
-     
-}
-
-//for managing add post
-if(isset($_GET['addpost'])){
-   $response = validatePostImage($_FILES['post_img']);
-
-   if($response['status']){
-if(createPost($_POST,$_FILES['post_img'])){
-    header("location:../../?new_post_added");
-}else{
-    echo "something went wrong";
-}
-   }else{
-    $_SESSION['error']=$response;
-    header("location:../../");
+   }
+   else
+   {
+       $_SESSION['error']=$response;
+       $_SESSION['formdata']=$_POST;
+       header("location:../../?daftar");
    }
 }
+
+//fungsi manajement login
+if(isset($_GET['login']))
+{
+    $response=validateLoginForm($_POST);
+
+   print_r($response);
+   if($response['status'])
+   {
+       $_SESSION['Auth'] = true;
+       $_SESSION['userdata'] = $response['user']; 
+
+       //ditambahkan setelah menggunakan PHPMailer
+       if($response['user']['ac_status']==0)
+       {
+            $_SESSION['code']= $code = rand(111111,999999);
+            sendCode($response['user']['email'],"Veirikasi Email Kamu",$code);
+       }
+
+       header("location:../../");
+
+
+   }
+   else
+   {
+       $_SESSION['error']=$response;
+       $_SESSION['formdata']=$_POST;
+       header("location:../../?login");
+   }
+}
+
+//kirim ulang kode
+if(isset($_GET['resend_code']))
+{
+   
+         $_SESSION['code']= $code = rand(111111,999999);
+         sendCode($_SESSION['userdata']['email'],"Veirikasi Email Kamu",$code);
+         header('location:../../?resended');
+}
+
+//Verifikasi Email dari kode
+if(isset($_GET['verify_email']))
+{
+   $user_code = $_POST['code'];
+   $code = $_SESSION['code'];
+   if($code==$user_code)
+   {
+    
+    if(verifyEmail($_SESSION['userdata']['email'])){
+        header('location:../../');
+    }
+    else
+    {
+        echo "ada yang salah (error)";
+    }
+   }
+   else
+   {
+       $response['msg']="Kode Verifikasi Salah!";
+
+       if(!$_POST['code'])
+       {
+           $response['msg']="Masukkan 6 Digit Code!";
+       }
+       $response['field']='email_verify';
+    $_SESSION['error']=$response;
+    header('location:../../');
+   }
+}
+
+//untuk Logout users
+if(isset($_GET['logout']))
+{
+    session_destroy();
+    header('location:../../');
+}
+
+//Lupa Password
+if(isset($_GET['lupapassword']))
+{
+    if(!$_POST['email'])
+    {
+        $response['msg']="Masukkan email kamu ";
+        $response['field']='email';
+        $_SESSION['error']=$response;
+        header('location:../../?lupaPassword');
+    }
+    elseif(!isEmailRegistered($_POST['email']))
+    {
+            $response['msg']="email tidak terdaftar";
+            $response['field']='email'; 
+            $_SESSION['error']=$response; 
+            header('location:../../?lupaPassword');
+    }
+    else
+    {
+        $_SESSION['lupa_email']=$_POST['email'];
+        $_SESSION['lupa_code']= $code = rand(111111,999999);
+        sendCode($_POST['email'],"Lupa Password ? ",$code);
+        header('location:../../?lupapassword&resended');
+    }
+}
+
+//verifikasi kode lupa password
+    if(isset($_GET['verifycode']))
+    {
+        $user_code = $_POST['code'];
+        $code = $_SESSION['lupa_code'];
+
+        if($code==$user_code)
+        {
+                $_SESSION['auth_temp'] = true;
+                header('location:../../');
+        }
+        else
+        {
+                $response['msg']="Kode Verifikasi Salah!";
+
+                if(!$_POST['code'])
+                {
+                    $response['msg']="Masukkan 6 Digit Code!";
+                }
+        
+                $response['field']='email_verify';
+                $_SESSION['error']=$response;
+                header('location:../../?lupapassword');
+        }
+}
+
+if(isset($_GET['ubahpassword']))
+{
+    if(!$_POST['password'])
+    {
+        $response['msg']="Masukkan password baru ";
+        $response['field']='password';
+        $_SESSION['error']=$response;
+        header('location:../../?lupapassword');
+    }
+    else
+    {
+    resetPassword($_SESSION['lupa_email'],$_POST['password']);
+    header('location:../../?reseted');
+    }
+}
+
+if(isset($_GET['updateprofile']))
+{
+    // echo "<pre>";
+    // print_r($_POST);
+    // print_r($_FILES);
+    $response=validateUpdateForm($_POST,$_FILES['profile_pic']);
+    // print_r($response);
+
+    if($response['status'])
+    {
+        if(updateProfile($_POST,$_FILES['profile_pic']))
+        {
+            header("location:../../?editprofile&success");
+        }
+        else
+        {
+            echo "ada yang salah";
+        }
+
+    
+    }else
+    {
+        $_SESSION['error']=$response;
+        $_SESSION['formdata']=$_POST;
+        header("location:../../?editprofile");
+    }
+}
+?>
